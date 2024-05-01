dc3823310b34be6a6e1031c0c1a29bb52bee64fd
kawleprasad03 <157042270+kawleprasad03@users.noreply.github.com>
2024-05-01 07:16:22 +0530

    commit

--- Bus_Booking/settings.py
+++ Bus_Booking/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9imxiz4np2)vb=iv6x3a!zp4kgda@u+7v-__*(=jjw)y0ja48o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
